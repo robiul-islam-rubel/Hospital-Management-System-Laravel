cc8a40d8bf4df71ecd0b427df4e3be2bf056a4b1
rubel <rubel.cse532@gmail.com>
2023-02-26 18:28:00 +0600

    admin panel added

---
+++ resources/views/admin/add_doctor.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   @include('admin.css');
+  </head>
+  <body>
+
+      @include('admin.sidebar');
+
+     @include('admin.navbar');
+     
+<div class="container-fluid page-body-wrapper">
+       
+    <h1>
+        Add Doctor
+</h1>
+@include('admin.script');
+
+  </body>
+</html>
