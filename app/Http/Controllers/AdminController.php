15f003fc2530b003e777137477c8ef8c50473785
rubel <rubel.cse532@gmail.com>
2023-02-26 23:36:15 +0600

    function implemented

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doctor;
 
 class AdminController extends Controller
 {
     public function addview()
     {
         return view('admin.add_doctor');
+    }
+
+    public function upload(Request $request)
+    {
+       $doctor= new doctor;
+       $image=$request->file;
+       $imagename=time().'.'.$image->getClientoriginalExtension();
+       $request->file->move('doctorimage',$imagename);
+
+       $doctor->image=$imagename;
+       $doctor->name=$request->name;
+       $doctor->phone=$request->phone;
+       $doctor->speciality=$request->speciality;
+       $doctor->room=$request->room;
+
+       $doctor->save();
+       return redirect()->back()->with('message','Doctor added successfully!');
+
+
     }
 }
